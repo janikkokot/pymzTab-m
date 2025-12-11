02b039d1bcbc09a0e66c5c972a6d41fb5bb97775
Janik Kokot <janik.kokot@i-med.ac.at>
2025-12-11 13:45:42 +0100

    Parametrize tests and adapt to explicit error and warning

--- tests/mztabm/test_mztabm.py
+++ tests/mztabm/test_mztabm.py
@@ -1,51 +1,22 @@
 import json
-from pathlib import Path
-import shutil
 import mztab_m_io
+import pytest
 
 
-def test_read_01():
+@pytest.mark.parametrize(
+    "file_path,format_",
+    [
+        ("tests/data/example/example.mztab", "tsv"),
+        ("tests/data/example/example.json", "json"),
+        ("tests/data/example/example.yaml", "yaml"),
+    ],
+)
+def test_read(file_path, format_):
     """
-    tsv file read
+    Test reading to a mzTab-M from several file formats
     """
-    file_path = "tests/data/example/example.mztab"
-    result: mztab_m_io.MzTabMLoadResult = mztab_m_io.read(file_path)
-    for message in result.messages:
-        print(message.message_type.name, message.message)
-
-    assert result.success
-    assert result.mztabm
-    mztabm_dict = mztab_m_io.convert_to_dict(result.mztabm)
-    assert mztabm_dict
-
-
-def test_read_02():
-    """
-    json file read
-    """
-    file_path = "tests/data/example/example.json"
-
-    result: mztab_m_io.MzTabMLoadResult = mztab_m_io.read(file_path, format="json")
-    for message in result.messages:
-        print(message.message_type.name, message.message)
-    assert result.success
-    assert result.mztabm
-    mztabm_dict = mztab_m_io.convert_to_dict(result.mztabm)
-    assert mztabm_dict
-
-
-def test_read_03():
-    """
-    yaml file read
-    """
-    file_path = "tests/data/example/example.yaml"
-
-    result: mztab_m_io.MzTabMLoadResult = mztab_m_io.read(file_path, format="yaml")
-    for message in result.messages:
-        print(message.message_type.name, message.message)
-    assert result.success
-    assert result.mztabm
-    mztabm_dict = mztab_m_io.convert_to_dict(result.mztabm)
+    mztabm: mztab_m_io.MzTabM = mztab_m_io.read(file_path, format=format_)
+    mztabm_dict = mztab_m_io.convert_to_dict(mztabm)
     assert mztabm_dict
 
 
@@ -60,57 +31,20 @@ def test_load_from_dict():
     assert mztabm_model
 
 
-def test_write_01():
+@pytest.mark.parametrize(
+    "file_path,format_",
+    [
+        ("tests/data/example/example.mztab", "tsv"),
+        ("tests/data/example/example.json", "json"),
+        ("tests/data/example/example.yaml", "yaml"),
+    ],
+)
+def test_write(file_path, format_, tmp_path):
     """
-    write an mztab-M model to tsv file
+    Test writing of an mztab-M model to several file formats
     """
-    file_path = "tests/data/example/example.mztab"
-
-    result: mztab_m_io.MzTabMLoadResult = mztab_m_io.read(file_path)
-    for message in result.messages:
-        print(message.message_type.name, message.message)
-    temp_folder = Path(".temp/mztabm")
-    target_path = temp_folder / Path("example.mztab")
-    try:
-        mztab_m_io.write(result.mztabm, str(target_path), format="tsv")
-    finally:
-        shutil.rmtree(temp_folder)
-        pass
-
-
-def test_write_02():
-    """
-    write an mztab-M model to json file
-    """
-    file_path = "tests/data/example/example.json"
-
-    result: mztab_m_io.MzTabMLoadResult = mztab_m_io.read(file_path, format="json")
-    for message in result.messages:
-        print(message.message_type.name, message.message)
-    temp_folder = Path(".temp/mztabm")
-    target_path = temp_folder / Path("example.json")
-    try:
-        mztab_m_io.write(result.mztabm, str(target_path), format="json")
-        # assert target_path.read_text() == Path(file_path).read_text()
-    finally:
-        shutil.rmtree(temp_folder)
-        pass
-
-
-def test_write_03():
-    """
-    write an mztab-M model to yaml file
-    """
-    file_path = "tests/data/example/example.yaml"
-
-    result: mztab_m_io.MzTabMLoadResult = mztab_m_io.read(file_path, format="yaml")
-    for message in result.messages:
-        print(message.message_type.name, message.message)
-    temp_folder = Path(".temp/mztabm")
-    target_path = temp_folder / Path("example.yaml")
-    try:
-        mztab_m_io.write(result.mztabm, str(target_path), format="yaml")
-        # assert target_path.read_text() == Path(file_path).read_text()
-    finally:
-        # shutil.rmtree(temp_folder)
-        pass
+    mztabm: mztab_m_io.MzTabM = mztab_m_io.read(file_path, format=format_)
+    target_path = tmp_path / "example"
+    mztab_m_io.write(mztabm, str(target_path), format=format_)
+    mztabm2: mztab_m_io.MzTabM = mztab_m_io.read(target_path, format=format_)
+    assert mztabm == mztabm2
