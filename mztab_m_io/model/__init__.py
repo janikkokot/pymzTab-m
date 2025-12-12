da39f528504829284582ad3342071326fbcc87d4
Janik Kokot <janik.kokot@i-med.ac.at>
2025-12-12 09:24:07 +0100

    Make serialize_object a method of MzTabBaseModel

--- mztab_m_io/model/__init__.py
+++ mztab_m_io/model/__init__.py
@@ -1,21 +1,28 @@
 import abc
+import warnings
 import enum
-from typing import Annotated, Any, Optional, Union
+from typing import (
+    Annotated,
+    Any,
+    Optional,
+    Union,
+)
 
 from pydantic import (
-    BaseModel,
-    ConfigDict,
+    AnyUrl,
     Field,
     SerializationInfo,
     SerializerFunctionWrapHandler,
+    BaseModel,
+    ConfigDict,
 )
+from pydantic.alias_generators import to_camel, to_pascal
 
 from mztab_m_io.model.serialization import (
     MetadataDictInfo,
     MetadataSerialization,
     SerializationContext,
 )
-from pydantic.alias_generators import to_camel, to_pascal
 
 
 class SerializationCategory(str, enum.Enum):
@@ -47,10 +54,87 @@ class MzTabBaseModel(BaseModel):
     def serialize_to_json(
         self, handler: SerializerFunctionWrapHandler, info: SerializationInfo
     ) -> tuple[bool, dict[str, Any]]:
+        warnings.warn(
+            "It is prefered to use `model_dump_json` instead.", DeprecationWarning
+        )
         if info and isinstance(info.context, SerializationContext):
             if info.context.convert_to and info.context.convert_to.lower() == "json":
                 return True, handler(self)
         return False, {}
+
+    def serialize(self, prefix: str) -> list[str]:
+        lines: list[str] = []
+        for field, field_info in self.__class__.model_fields.items():
+            serializer_model = field_info.json_schema_extra or {}
+            extra = MetadataSerialization.model_validate(serializer_model)
+            if extra.ignore:
+                continue
+            alias = field_info.alias if field_info.alias else field
+            key_name = alias
+            if prefix:
+                key_name = prefix if extra.object_level_value else f"{prefix}-{alias}"
+
+            try:
+                value = getattr(self, field)
+            except AttributeError:
+                continue
+            if isinstance(value, (str, AnyUrl)):
+                line = f"{key_name}\t{value or ''}"
+                lines.append(line)
+            elif isinstance(value, int):
+                if extra.referenced_field_name:
+                    ref = extra.referenced_field_name
+                    line = f"{key_name}\t{ref}[{value}]"
+                else:
+                    line = f"{key_name}\t{value}"
+                lines.append(line)
+            elif isinstance(value, MzTabBaseModel):
+                lines.extend(value.serialize(prefix=key_name))
+            elif isinstance(value, list):
+                if not value:
+                    continue
+                if isinstance(value[0], (str, AnyUrl)):
+                    separator = extra.list_concatenation_str
+                    if separator:
+                        line_value = separator.join([str(x) for x in value])
+                        line = f"{key_name}\t{line_value}"
+                    else:
+                        for idx, item in enumerate(value, start=1):
+                            indexed_key_name = f"{key_name}[{idx}]"
+                            line = f"{indexed_key_name}\t{item or ''}"
+                            lines.append(line)
+                elif isinstance(value[0], int):
+                    separator = extra.list_concatenation_str or "|"
+                    if extra.referenced_field_name:
+                        values = [f"{extra.referenced_field_name}[{x}]" for x in value]
+                        line_value = separator.join(values)
+                        line = f"{key_name}\t{line_value}"
+                    else:
+                        values = [str(x) for x in value if x is not None]
+                        line = f"{key_name}\t{separator.join(values)}"
+                    lines.append(line)
+                elif isinstance(value[0], MzTabBaseModel):
+                    separator = extra.list_concatenation_str or None
+                    if separator:
+                        line_value = separator.join([str(x) for x in value])
+                        line = f"{key_name}\t{line_value or ''}"
+                        lines.append(line)
+                    else:
+                        for idx, item in enumerate(value, start=1):
+                            indexed_key_name = f"{key_name}[{idx}]"
+                            if extra.non_indexed_list_value:
+                                indexed_key_name = key_name
+                            elif isinstance(item, IdentifiableModel):
+                                id_val = item.get_id()
+                                if id_val:
+                                    indexed_key_name = f"{key_name}[{id_val}]"
+                            lines.extend(item.serialize(prefix=indexed_key_name))
+                else:
+                    warnings.warn("not expected")
+
+            else:
+                warnings.warn(f"Skipping unsupported value {key_name!r} {extra!r}")
+        return lines
 
 
 class SerializableModel(MzTabBaseModel):
